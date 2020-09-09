d08d186203b4f746e4ce5d9b3a8bc06d2f669fb3
Dimas <stupidjsdev@gmail.com>
2020-09-09 11:38:25 +0300

    fixed dotenv

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,23 @@
 import axios from "axios";
-const dotenv = import('dotenv').config()
 
-const serverUrl = process.env.API_URL
 const config = axios.create({
-  baseURL: serverUrl, // TODO: add to .env
+  baseURL: `http://localhost:8000`, // TODO: add to .env
   withCredentials: true,
 });
-
 const apiUrls = {
-  products: `${serverUrl}/products`,
+  products: `/products`,
 }
-
 export const productsAPI = {
   getAllProducts() {
-
-    return config.get(apiUrls.products).then((response) => response.data);
+    return config.get(`products`).then((response) => response.data);
   },
   getProductByID(id) {
     console.log(id);
-    return config.get(`${apiUrls.products}${id}`).then((response) => response.data);
+    return config.get(`products/${id}`).then((response) => response.data);
   },
   addProduct(name, price, isAvailable) {
-    console.log(name, price, isAvailable);
     return config
-      .post(`${apiUrls.products}`, {
+      .post( `products`,{
         name,
         price,
         isAvailable,
@@ -31,9 +25,9 @@ export const productsAPI = {
       .then(() => console.log('done'));
   },
   editProduct(id, data) {
-    return config.put(`${apiUrls.products}${id}`, data).then((response) => response.data);
+    return config.put(`products/${id}`, data).then((response) => response.data);
   },
   deleteProductById(id) {
-    return config.delete(`${apiUrls.products}${id}`).then((response) => response.data);
+    return config.delete(`products/${id}`).then((response) => response.data);
   },
 };
