99a8047375385d7e12f394ca4563f4d323f72b72
Dimas <stupidjsdev@gmail.com>
2020-09-09 11:41:30 +0300

    a little bit fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,26 @@
 import axios from "axios";
 
+
+
 const config = axios.create({
   baseURL: `http://localhost:8000`, // TODO: add to .env
   withCredentials: true,
 });
 const apiUrls = {
-  products: `/products`,
+  products: `/products/`,
 }
 export const productsAPI = {
   getAllProducts() {
-    return config.get(`products`).then((response) => response.data);
+    return config.get(apiUrls.products).then((response) => response.data);
   },
   getProductByID(id) {
     console.log(id);
-    return config.get(`products/${id}`).then((response) => response.data);
+    return config.get(`${apiUrls.products}${id}`).then((response) => response.data);
   },
   addProduct(name, price, isAvailable) {
+    console.log(name, price, isAvailable);
     return config
-      .post( `products`,{
+      .post(`${apiUrls.products}`, {
         name,
         price,
         isAvailable,
@@ -25,9 +28,9 @@ export const productsAPI = {
       .then(() => console.log('done'));
   },
   editProduct(id, data) {
-    return config.put(`products/${id}`, data).then((response) => response.data);
+    return config.put(`${apiUrls.products}${id}`, data).then((response) => response.data);
   },
   deleteProductById(id) {
-    return config.delete(`products/${id}`).then((response) => response.data);
+    return config.delete(`${apiUrls.products}${id}`).then((response) => response.data);
   },
 };
