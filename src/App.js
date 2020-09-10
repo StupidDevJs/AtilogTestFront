0a12d5a3f6d19209a9bf0fe6161e4ad0be96411c
Dimas <stupidjsdev@gmail.com>
2020-09-10 11:05:52 +0300

    a little bit fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import {routes} from './routes'
+import "./App.css";
 
 export const App = () => {
-    const routers = routes.map((item,i) =>{
+    const routers = routes.map((item) =>{
         return <Route path={item.path} component={item.component} exact={item.exact} key={item.key}/>
     })
   return (
