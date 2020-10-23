73767b19b6fb68a53721dd3058403b0e5a6c99f9
Dimas <stupidjsdev@gmail.com>
2020-10-23 18:53:58 +0300

    bugs fixed

--- src/view/Auth/SignUp.js
+++ src/view/Auth/SignUp.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { RegisterForm } from "../../components/RegisterForm";
-import { connect } from "react-redux";
 
 class SignUp extends Component {
     render() {
-        const signUpRequest = this.props.signUpRequest
         return (
             <RegisterForm submit={signUpRequest} title={'Sign Up'} />
         )
